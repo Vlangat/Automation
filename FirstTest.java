0c848e797af0cba29b48a0bb5122e91848af4af4
Vlangat <98968536+Vlangat@users.noreply.github.com>
2023-11-13 22:43:06 +0300

    Add files via upload

---
+++ FirstTest.java
@@ -0,0 +1,37 @@
+package qaexamples;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class FirstTest {
+
+    WebDriver driver;
+    @BeforeClass
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(50, TimeUnit.MILLISECONDS);
+    }
+    @Test(groups = {"smoke"})
+    public void login(){
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+    }
+    @AfterTest
+    public void close(){
+
+
+    }
+}
+
---
+++ MultiBrowser.java
@@ -0,0 +1,26 @@
+package qaexamples;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+public class MultiBrowser {
+    public WebDriver driver;
+
+    @Parameters("browser")
+    @BeforeClass
+    public void setup(String browser){
+        if (browser.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+
+        }
+
+    }
+}
